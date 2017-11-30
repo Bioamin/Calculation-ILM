a638246b2ce0cb777be29d929d2b4bbae49b8448
Bioamin <aboroomand@ucmerced.edu>
2017-11-30 00:22:28 -0800

    This code make a new file out of ILMpy result file. That new file contain the median of  2nd and 3rd functional load for each Trial

---
+++ Median_file_maker.py
@@ -0,0 +1,60 @@
+ 
+################################################################################                                                                                                                           
+#Amin Boroomand 2017
+# GOAL: compute the average functional load for each dimention  
+#INPUTS:2nd and 3rd functional load ratio
+################################################################################
+import os
+import pandas as pd
+from copy import deepcopy
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+import glob
+import os
+from pandas import *
+
+################################################################################  
+TrialNum=20         #type number of trials
+GenerNum=5          #type number of generation
+indexlist=[]
+
+med=((GenerNum+1)/2)-1
+for i in range(TrialNum):
+    indx=med+(i*GenerNum)
+    indexlist.append(indx)
+#print("computing the index of median for each Trial")
+
+
+WDadrs= '/home/aboroomand/ILMresults/'
+os.chdir(WDadrs)
+dataList = glob.glob(r'*.tsv')
+
+
+#reading the file names
+nameList = []
+dataDct = {}
+for lines in dataList:
+    path_list = lines.split(os.sep)
+    name = path_list
+    for filename in name:
+        dataDct[filename]=pd.read_csv(filename, sep='\t',  names = ["second","third"]) 
+
+
+for lines in dataList:
+    dictdf=dataDct[lines]  #All data in a file
+    line_name=lines[:-41]  #noise rate that extracted from file name
+    filename=lines[:-4]
+    sec_med=dictdf["second"].median()
+    Third_med=dictdf["third"].median()
+    
+    second_column=dictdf["second"]
+    Third_column=dictdf["third"]
+
+    newdf=dictdf.ix[indexlist]
+    newdf.to_csv(filename+'_median_t100_G21.tsv', sep='\t', index=None, header=None)
+    sec_med=newdf["second"].median()
+    Third_med=newdf["third"].median()
+    
+    print("Noise rate= ",line_name, "    med 2nd dimention= ", sec_med,"   median 3rd dim=", Third_med)
+
